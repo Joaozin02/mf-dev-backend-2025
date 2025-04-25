880750decc27a5df733665fb74842aa55c25b730
João <joao_rezende02@hotmail.com>
2025-04-25 16:00:53 -0300

    CRUD Veículo

--- mf-dev-backend-2025/Models/Veiculo.cs
+++ mf-dev-backend-2025/Models/Veiculo.cs
@@ -19,11 +19,13 @@ namespace mf_dev_backend_2025.Models
         public string Placa { get; set; }
 
         [Required(ErrorMessage = "Obrigatório informar o ano de fabricação!")]
+        [Display(Name = "Ano de Fabricação")]
         public int AnoFabricacao{ get; set; }
 
-
         [Required(ErrorMessage = "Obrigatório informar o ano do modelo!")]
+        [Display(Name = "Ano do Modelo")]
         public int AnoModelo {  get; set; }
+
         
 
     }
